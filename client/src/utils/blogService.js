ed58b2f38ddea2b23082fc71587ec65c03a98782
vandhanchowdhary <vandhanchowdhary@gmail.com>
2025-09-28 20:34:51 +0530

    Add backend URL from Render

--- client/src/utils/blogService.js
+++ client/src/utils/blogService.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE =
+  import.meta.env.VITE_API_URL || "https://mern-blog-jj4u.onrender.com";
 
 // --- Public APIs ---
 export const getAllBlogs = async (page = 1) => {
